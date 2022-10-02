cbb6414336eb20a2f1e4628d98ef0d870e43217a
Ibrahim Samandar <ibrahimisamsamandar@gmail.com>
2022-10-02 18:55:27 -0500

    Moving to routes - Test

--- backend/routes/eventsData.js
+++ backend/routes/eventsData.js
@@ -4,6 +4,11 @@ const router = express.Router();
 //importing data model schemas
 let { eventdata } = require("../models/models"); 
 
+//2 months ago
+let date = new Date();
+(date.setMonth(date.getMonth() - 2));
+let dateInput = date.toLocaleDateString();
+
 //GET all entries
 router.get("/", (req, res, next) => { 
     eventdata.find( 
@@ -122,6 +127,19 @@ router.put("/addAttendee/:id", (req, res, next) => {
         }
     );
     
+});
+//endpoint that creates Event Document with how many attendees that signed up for each individual event in last 2 months
+router.get('/eventSignUp', ( res, next) => {
+    eventdata.aggregate([
+      { $match : { $gte: [{ $toDate: dateInput }, "$date"]}},
+      { $project : { _id : 0, eventName : 1, services : 0, date : 1, address : 0, description:0, count : {$size: '$attendees'}}},
+    ], (error, data) => {
+      if (error) {
+        return next(error)
+      } else {
+        res.json(data);
+      }
+    });
 });
 
 module.exports = router;
