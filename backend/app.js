2749b1ab159415f7b027a957a1e5c704dcebe872
Ibrahim Samandar <ibrahimisamsamandar@gmail.com>
2022-10-02 16:25:10 -0500

    basic structure for aggregation

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,11 @@ app.use(morgan("dev"));
 const primaryDataRoute  = require('./routes/primaryData');
 const eventsDataRoute  = require('./routes/eventsData');
 
+//2 months ago
+let date = new Date();
+(date.setMonth(date.getMonth() - 2));
+let dateInput = date.toLocaleDateString();
+
 //setup middle ware for routes
 app.use('/primaryData', primaryDataRoute);
 app.use('/eventData', eventsDataRoute)
@@ -41,6 +46,20 @@ app.use('/eventData', eventsDataRoute)
 app.listen(PORT, () => {
   console.log("Server started listening on port : ", PORT);
 });
+//endpoint that creates Event Document with how many attendees that signed up for each individual event in last 2 months
+app.get('/eventSignUp', (res, next) => {
+  eventsDataRoute.aggregate([
+    { $match : { $gte: [{ $toDate: dateInput }, "$date"]}},
+    { $project : { _id : 0, eventName : 1, services : 0, date : 1, address : 0, description:0, count : {$size: '$attendees'}}},
+  ], (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data);
+    }
+  });
+});
+
 
 //error handler
 app.use(function (err, req, res, next) {
