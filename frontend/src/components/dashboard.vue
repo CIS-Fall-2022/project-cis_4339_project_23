d76eaaf5ed093527829538f552cc669bf8bfcca2
Ibrahim Samandar <ibrahimisamsamandar@gmail.com>
2022-10-22 15:47:46 -0500

    Fixing spacing on event table

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -4,21 +4,21 @@
       <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome</h1>
     </div>
     <!-- Display Found Data -->
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 gap-x-6 gap-y-10">
-      <div class="flex flex-col col-span-2">
+    <div class="grid-cols-1 sm:grid-cols-2 md:grid-cols-1 gap-x-6 gap-y-10">
+      <div class="flex-col col-span-2">
         <table class="min-w-full shadow-md rounded">
           <thead class="bg-gray-50 text-xl">
             <tr>
-              <th class="p-5 text-left">Event Name</th>
-              <th class="p-5 text-left">Event Date</th>
-              <th class="p-5 text-left">Number Of Attendees</th>
+              <th class="p-1 text-left">Event Name</th>
+              <th class="p-5 text-center">Event Date</th>
+              <th class="p-5 text-center">Number Of Attendees</th>
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-300">
             <tr v-for="event in queryData" :key="event._id">
               <td class="p-2 text-left">{{ event.eventName }}</td>
-              <td class="p-2 text-left">{{ formattedDate(event.date) }}</td>
-              <td class="p-2 text-left">{{ event.numberofattendees }}</td>
+              <td class="p-2 text-center">{{ formattedDate(event.date) }}</td>
+              <td class="p-2 text-center">{{ event.numberofattendees }}</td>
             </tr>
           </tbody>
         </table>
