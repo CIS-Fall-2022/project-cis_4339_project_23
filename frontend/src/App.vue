fae161432f3c9ea57385ecc2f41f51340f6a23dc
Ibrahim Samandar <ibrahimisamsamandar@gmail.com>
2022-11-16 12:23:13 -0600

    Code Cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -45,7 +45,7 @@
       <section
         class="justify-end items-center h-24 flex"
         style="background: linear-gradient(250deg, #C8102E 70%, #efecec 50.6%)">
-		<envnameVue class="mr-20 text-3xl text-white"></envnameVue>
+		    <envnameVue class="mr-20 text-3xl text-white"></envnameVue>
       </section>
       <div>
         <router-view></router-view>
@@ -63,11 +63,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#_container {
-  background-color: #c8102e;
-  color: white;
-  padding: 18px;
-}
-</style>
